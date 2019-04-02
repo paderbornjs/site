9abc2b3554ce9d2b748612eb3324b6e6cbb33ded
Christoph Werner <christoph+github@codepunkt.de>
2019-04-02 11:50:51 +0200

    refactor: test util to render inside providers

--- packages/client/src/components/About/About.test.tsx
+++ packages/client/src/components/About/About.test.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { render } from 'react-testing-library'
-import { ThemeProvider } from 'styled-components/macro'
-import theme from '../../style/theme'
+import { render } from '../../testUtils'
 import About from './About'
 
 describe('<About />', () => {
   test('matches snapshot', () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <About />
-      </ThemeProvider>
-    )
+    const { container } = render(<About />)
 
     expect(container.firstChild).toMatchSnapshot()
   })
