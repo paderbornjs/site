ee4c4cdf5f62721bae02ddc9d771b5cea5e6b872
Christoph Werner <christoph@codepunkt.de>
2019-04-07 11:57:20 +0200

    chore: add typeahead plugins to toplevel test watcher

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   projects: ['<rootDir>/packages/api', '<rootDir>/packages/client'],
-  watchPlugins: ['jest-watch-select-projects'],
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
