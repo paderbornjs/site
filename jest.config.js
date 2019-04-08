e05dc0f189e555bc41c814527e5c023432534fa0
Christoph Werner <christoph+github@codepunkt.de>
2019-04-08 15:45:55 +0200

    chore: fix single and multi project test running, hopefully

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   projects: ['<rootDir>/packages/api', '<rootDir>/packages/client'],
   watchPlugins: [
-    'jest-watch-select-projects',
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
